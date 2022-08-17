8f8602c0fc1808b79330b8fb5fa0e17d4f93497d
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-08-17 14:59:53 +0630

    added user register & delete

--- src/pages/StudentsPage.jsx
+++ src/pages/StudentsPage.jsx
@@ -1,13 +1,69 @@
 import { Navbar } from '../components/Navbar';
 import { Main } from '../components/Main';
+import { useEffect , useState } from 'react';
 
 export const StudentsPage = () => {
+
+    const [ students , setStudents ] = useState([]);
+    const [ courses , setCourses ] = useState([]);
+
+    const fetchCourses = () => {
+      fetch("http://localhost:3000/courses")
+      .then( res => res.json())
+      .then( datas => setCourses(datas));
+    }
+
+    const fetchStudents = () => {
+      fetchCourses();
+      fetch("http://localhost:3000/students")
+      .then( res => res.json())
+      .then( datas => setStudents(datas) );
+    }
+
+
+    useEffect(() => {
+      fetchStudents();
+    } , [] );
+
     return (
         <>
           <Navbar />
           
           <Main>
-            <h1>students pages</h1>
+
+            <h3 className="h3 mx-5 my-3 px-3 text-center">Students Management</h3>
+        
+            <table className="mx-auto my-5 table table-striped w-75  table-bordered">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Joined Courses</th>
+                        <th>Detail</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { students.map( stu => {
+                        return (
+                            <tr key={stu.id}>
+                                <td>{stu.id}</td>
+                                <td className='fw-bold'>{stu.name}</td>
+                                <td className='gap-1'>
+                                  { stu.attendCourses.map( c => {
+                                    return courses.filter( course => course.id == c ).map( result => {
+                                      return <span className='btn btn-sm btn-success' key={result.id}>{result.name}</span>
+                                    })
+                                  })}
+                                </td>
+                                <td>
+                                  <a href="" className="btn btn-success btn-sm">Detail</a>
+                                </td>
+                            </tr>
+                        )
+                    }) }
+                </tbody>
+            </table>
+
           </Main>
         </>
     )
--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,13 +1,57 @@
 import { Navbar } from '../components/Navbar';
 import { Main } from '../components/Main.jsx';
+import { useEffect, useState } from 'react';
 
 export const UsersPage = () => {
+
+    const [ users , setUsers ] = useState([]);
+
+    function fetchUsers()
+    {
+        fetch("http://localhost:3000/users")
+        .then( res => res.json())
+        .then( datas => {
+            setUsers(datas);
+        })
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    } , [] );
+    
+
     return (
-        <>
+        <>  
          <Navbar />
         
          <Main>
-            <h1>users page</h1>
+            
+            <h3 className="h3 mx-5 my-3 px-3 text-center">Users Management</h3>
+        
+            <table className="mx-auto my-5 table table-striped w-75  table-bordered">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { users.map( user => {
+                        return (
+                            <tr key={user.id}>
+                                <td>{user.id}</td>
+                                <td className='fw-bold'>{user.name}</td>
+                                <td className='gap-1'>
+                                    <a href="" className="btn btn-success btn-sm mx-1">Update</a>
+                                    <a href="" className="btn btn-danger btn-sm mx-1">Delete</a>
+                                </td>
+                            </tr>
+                        )
+                    }) }
+                </tbody>
+            </table>
+           
          </Main>
         </>
     )
